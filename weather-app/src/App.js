4457133ff5f202ef38c0a7f01bb126df0bcaf51b
Dmytro-Chornomaz <bloody_justice@outlook.com>
2023-11-14 20:01:14 +0100

    Created weather-app

---
+++ weather-app/src/App.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+function App() {
+
+    const [data, setData] = useState({});
+    const [town, setTown] = useState("");
+
+    const key = "9e3a7d4c14e2232663202f4152854f9a";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${town}&units=metric&appid=${key}`;
+
+    function searchWeather(event) {
+        if (event.key === "Enter") {
+            fetch(url)
+                .then(response => response.json())
+                .then(json => console.log(json));
+        }
+    }
+
+    return (
+        <div className="App">
+            <div className="inp-field">
+                <input type="text" value={town} onChange={event => setTown(event.target.value)}
+                    placeholder="Enter your town" onKeyDown={searchWeather} />
+            </div>
+        </div>
+    );
+}
+
+export default App;
