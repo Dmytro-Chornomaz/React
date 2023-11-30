35bf1c30eb116e0051c89a99999cc997b352d59f
Dmytro-Chornomaz <bloody_justice@outlook.com>
2023-11-30 16:07:17 +0100

    Added LogIn and LogOut to header

--- finance_organizer/src/Pages/Header/Header.js
+++ finance_organizer/src/Pages/Header/Header.js
@@ -1,9 +1,92 @@
 import './Header.css';
+import { useState, useRef } from 'react';
 
 function Header() {
+
+    const [visibilityUserInfo, setVisibilityUserInfo] = useState({ display: "none" }); // none
+    const [visibilityLoginForm, setVisibilityLoginForm] = useState({ display: "block" }); // block
+    const [userName, setUserName] = useState("");
+
+    const inputName = useRef();
+    const inputPassword = useRef();
+
+    let wrongInputMessage = false;
+    let smthWrongMessage = false;
+
+    // To delete login mistakes messages when the input field receives focus.
+    function removeRedMessages() {
+        wrongInputMessage = false;
+        smthWrongMessage = false;
+    }
+
+    // Log in function.
+    async function logInBtnHandler() {
+        const response = await fetch("https://localhost:7203/api/Login/GenerateToken", {
+            method: "POST",
+            headers: {
+                "Accept": "text/plain",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                login: inputName.current.value,
+                password: inputPassword.current.value
+            })
+        });
+
+        if (response.ok === true) {
+            const data = await response.json();
+            sessionStorage.setItem("userName", data.userName);
+            sessionStorage.setItem("accessToken", data.accessToken);
+            inputName.current.value = "";
+            inputPassword.current.value = "";
+            setVisibilityLoginForm({ display: "none" });
+            setVisibilityUserInfo({ display: "block" });
+            setUserName(data.userName);
+        }
+        else if (response.status === 401) {
+            wrongInputMessage = true;
+            inputName.current.value = "";
+            inputPassword.current.value = "";
+        }
+        else {
+            smthWrongMessage = true;
+            inputName.current.value = "";
+            inputPassword.current.value = "";
+        }
+    }
+
+    // Log out function.
+    function logOutBtnHandler() {
+        sessionStorage.removeItem("userName");
+        sessionStorage.removeItem("accessToken");
+        setUserName("");
+        setVisibilityUserInfo({ display: "none" });
+        setVisibilityLoginForm({ display: "block" });
+    }
+
     return (
         <div className="Header">
-            <h3>Finance Organizer</h3>            
+            <h3 className="Name">Finance Organizer</h3>
+            <div className="Login">
+                <div className="userInfo" style={visibilityUserInfo} >
+                    <p className="userInfoText">You are logged in as {userName}</p>
+                    <input type="button" value="Log out" onClick={logOutBtnHandler} />
+                    <input type="button" value="Delete account" />
+                </div>
+                {wrongInputMessage && <p className="wrongInputMessage">Wrong name or/and password!</p>}
+                {smthWrongMessage && <p className="smthWrongMessage">Oops! Something went wrong!</p>}
+                <div className="loginForm" style={visibilityLoginForm} >
+                    <h4 className="LogInText">Log in or create account</h4>
+                    <p>
+                        <input type="text" placeholder="Input your name" ref={inputName} onFocus={removeRedMessages} />
+                    </p>
+                    <p>
+                        <input type="password" placeholder="Input your password" ref={inputPassword} onFocus={removeRedMessages} />
+                    </p>
+                    <input type="button" value="Log in" onClick={logInBtnHandler} />
+                    <input type="button" value="Create account" />
+                </div>
+            </div>
         </div>
     );
 }
--- finance_organizer/src/Pages/Welcome/Welcome.js
+++ finance_organizer/src/Pages/Welcome/Welcome.js
@@ -1,55 +1,81 @@
 import './Welcome.css';
-import { useState, useRef } from 'react';
+//import { useState, useRef } from 'react';
 
 export default function Welcome() {
 
-    let tokenKey = "accessToken";
+    //const [visibilityUserInfo, setVisibilityUserInfo] = useState({ display: "none" });
+    //const [visibilityLoginForm, setVisibilityLoginForm] = useState({ display: "block" });
+    //const [userName, setUserName] = useState("");
 
-    const [visibility, setVisibility] = useState({ display: "none" });
-    const [userName, setUserName] = useState("");
+    //const inputName = useRef();
+    //const inputPassword = useRef();
 
-    const inputName = useRef();
-    const inputPassword = useRef();
+    //let wrongInputMessage = false;
+    //let smthWrongMessage = false;
 
-    async function logInBtnHandler() {
-        const response = await fetch("https://localhost:7203/api/Login/GenerateToken", {
-            method: "POST",
-                headers: { 
-				"Accept": "text/plain", 
-				"Content-Type": "application/json",
-//"Access-Control-Allow-Origin": "https://localhost:7203"
-				},
-                body: JSON.stringify({
-                    login: inputName.current.value,
-                    password: inputPassword.current.value
-                })
-        });
+    //// To delete login mistakes messages when the input field receives focus.
+    //function removeRedMessages() {
+    //    wrongInputMessage = false;
+    //    smthWrongMessage = false;
+    //}
 
-        if (response.ok === true) {
-            const data = await response.json();
-            console.log(data);
-        }
-    }
+    //// Login function.
+    //async function logInBtnHandler() {
+    //    const response = await fetch("https://localhost:7203/api/Login/GenerateToken", {
+    //        method: "POST",
+    //        headers: {
+    //            "Accept": "text/plain",
+    //            "Content-Type": "application/json",
+    //        },
+    //        body: JSON.stringify({
+    //            login: inputName.current.value,
+    //            password: inputPassword.current.value
+    //        })
+    //    });
+
+    //    if (response.ok === true) {
+    //        const data = await response.json();
+    //        sessionStorage.setItem("userName", data.userName);
+    //        sessionStorage.setItem("accessToken", data.accessToken);
+    //        inputName.current.value = "";
+    //        inputPassword.current.value = "";
+    //        setVisibilityLoginForm({ display: "none" });
+    //        setVisibilityUserInfo({ display: "block" });
+    //        setUserName(data.userName);
+    //    }
+    //    else if (response.status === 401) {
+    //        wrongInputMessage = true;
+    //        inputName.current.value = "";
+    //        inputPassword.current.value = "";
+    //    }
+    //    else {
+    //        smthWrongMessage = true;
+    //        inputName.current.value = "";
+    //        inputPassword.current.value = "";
+    //    }
+    //}
 
     return (
         <div>
             <h1>Welcome</h1>
-            <div className="userInfo" style={visibility} >
-                <p>You are signed in as {userName}</p>
-                <input type="button" value="Log out" id="logOut" />
-            </div>
-            <div className="loginForm">
-                <h3>Log in to the service</h3>
-                <p>
-                    <label>Input your name</label><br />
-                    <input type="text" ref={inputName} />
-                </p>
-                <p>
-                    <label>Input your password</label><br />
-                    <input type="password" ref={inputPassword} />
-                </p>
-                <input type="button" value="Log in" onClick={logInBtnHandler} />
-            </div>
+            {/*<div className="userInfo" style={visibilityUserInfo} >*/}
+            {/*    <p>You are signed in as {userName}</p>*/}
+            {/*    <input type="button" value="Log out" />*/}
+            {/*</div>*/}
+            {/*{wrongInputMessage && <p className="wrongInputMessage">Wrong name or/and password!</p>}*/}
+            {/*{smthWrongMessage && <p className="smthWrongMessage">Oops! Something went wrong!</p> }*/}
+            {/*<div className="loginForm" style={visibilityLoginForm} >*/}
+            {/*    <h3>Log in to the service</h3>*/}
+            {/*    <p>*/}
+            {/*        <label>Input your name</label><br />*/}
+            {/*        <input type="text" ref={inputName} onFocus={removeRedMessages} />*/}
+            {/*    </p>*/}
+            {/*    <p>*/}
+            {/*        <label>Input your password</label><br />*/}
+            {/*        <input type="password" ref={inputPassword} onFocus={removeRedMessages} />*/}
+            {/*    </p>*/}
+            {/*    <input type="button" value="Log in" onClick={logInBtnHandler} />*/}
+            {/*</div>*/}
         </div>
     );
 }
