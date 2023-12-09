79b581911be94ff2fb7a2ec014e221794a974ece
Dmytro-Chornomaz <bloody_justice@outlook.com>
2023-12-09 17:51:14 +0100

    Added validation to AddTransaction

--- finance_organizer/src/Pages/AddTransaction/AddTransaction.js
+++ finance_organizer/src/Pages/AddTransaction/AddTransaction.js
@@ -12,19 +12,100 @@ export default function AddTransaction() {
     const summaryExpenses = useRef();
     const savings = useRef();
 
+    function addBtnHandler() {
+
+        let inputsArray = [
+            meal,
+            communalServices,
+            medicine,
+            transport,
+            purchases,
+            leisure,
+            summaryExpenses,
+            savings
+        ];
+
+        let answer = inputValidator(inputsArray);
+        console.log(answer);
+
+    }
+
+    function inputValidator(arr) {
+
+        let validationFail;        
+        let zeroCounter = 0;        
+
+        for (let item of arr) {
+
+            item.current.style.color = "";
+
+            if (item.current.value === "" || item.current.value === "0") {
+                item.current.value = 0;
+                zeroCounter++;
+            }
+
+            if (!validateNumber(item.current.value)) {
+                item.current.style.color = "red";
+                validationFail = true;
+            }
+        }
+        
+        if (validationFail === true || zeroCounter > 7) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    function validateNumber(input) {
+
+        let result;
+
+        if (input.includes(",")) {
+            result = false;
+            return result;
+        }
+
+        const number = parseFloat(input);
+                
+        if (isNaN(number) === true) {
+            result = false;
+        }
+        else if (number > 1000000000 || number < 0) {
+            result = false;
+        }
+        else if (number - roundToTwoDecimalPlaces(number) !== 0) {
+            result = false;
+        }
+        else {
+            result = true;
+        }
+
+        return result;        
+    }
+
+    function roundToTwoDecimalPlaces(number) {
+        return Math.round(number * 100) / 100;
+    }
+
+    function colorReset(e) {
+        e.target.style.color = "";
+    }
+
     return (
         <div>
             <h1>Add Transaction</h1>
             <div className="inputForm">
-                <p><input type="text" placeholder="Meal" ref={meal} /></p>
-                <p><input type="text" placeholder="Communal services" ref={communalServices} /></p>
-                <p><input type="text" placeholder="Medicine" ref={medicine} /></p>
-                <p><input type="text" placeholder="Transport" ref={transport} /></p>
-                <p><input type="text" placeholder="Purchases" ref={purchases} /></p>
-                <p><input type="text" placeholder="Leisure" ref={leisure} /></p>
-                <p><input type="text" placeholder="Summary expenses" ref={summaryExpenses} /></p>
-                <p><input type="text" placeholder="Savings" ref={savings} /></p>
-                <p><input type="button" value="Add transaction" /></p>
+                <p><input type="text" onFocusCapture={colorReset} placeholder="Meal" ref={meal} /></p>
+                <p><input type="text" onFocusCapture={colorReset} placeholder="Communal services" ref={communalServices} /></p>
+                <p><input type="text" onFocusCapture={colorReset} placeholder="Medicine" ref={medicine} /></p>
+                <p><input type="text" onFocusCapture={colorReset} placeholder="Transport" ref={transport} /></p>
+                <p><input type="text" onFocusCapture={colorReset} placeholder="Purchases" ref={purchases} /></p>
+                <p><input type="text" onFocusCapture={colorReset} placeholder="Leisure" ref={leisure} /></p>
+                <p><input type="text" onFocusCapture={colorReset} placeholder="Summary expenses" ref={summaryExpenses} /></p>
+                <p><input type="text" onFocusCapture={colorReset} placeholder="Savings" ref={savings} /></p>
+                <p><input type="button" value="Add transaction" onClick={addBtnHandler} /></p>
             </div>
         </div>
     );
